3741eeb53d9a0c30f67914da3fc81578c754061e
Jordan <jordan@burdett.us>
2019-10-02 15:51:40 -0600

    test

--- index.php
+++ index.php
@@ -8,22 +8,35 @@
 <html>
 
 <head>
-    <title></title>
+    <title>Order page</title>
     <link rel="stylesheet" href="styles.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/5990b2791e.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
+    <div class="container-fluid">
+        <h1 class="display-4">Order Here!</h1>
+    </div>
+
+
     <div class="container">
-        <button type="button" class="btn btn-primary" onclick="storeInfo('banana')">banana</button>
-        <button type="button" class="btn btn-primary" onclick="storeInfo('mango')">mango</button>
-        <button type="button" class="btn btn-primary" onclick="storeInfo('pear')">pear</button>
-        <button type="button" class="btn btn-primary" onclick="storeInfo('apple')">apple</button>
-        <button type="button" class="btn btn-primary" onclick="storeInfo('peach')">peach</button>
+        <div class="col">
+            <button type="button" class="btn btn-primary" onclick="storeInfo('banana')"> Add to Cart</button>
+            <button type="button" class="btn btn-primary" onclick="storeInfo('mango')" > Add to Cart</button>
+            <button type="button" class="btn btn-primary" onclick="storeInfo('pear')"  > Add to Cart</button>
+            <button type="button" class="btn btn-primary" onclick="storeInfo('apple')" > Add to Cart</button>
+            <button type="button" class="btn btn-primary" onclick="storeInfo('peach')" > Add to Cart</button>
+        </div>
+
+        <div class="col-sm-2">
+        <i class="fas fa-shopping-cart"><h4>Cart</h4></i>
+            
+        </div>
 
         <a href="ShowSession.php">show session</a>
     </div>
