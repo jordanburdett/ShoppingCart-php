cd978a59718815f469ad38c891c3396de4597b32
Jordan <jordan@burdett.us>
2019-10-02 15:10:10 -0600

    test

--- javascript.js
+++ javascript.js
@@ -2,6 +2,6 @@
 /************
  * quick ajax request using jquery to store item to the session
  */
-function storeInfo(key, item) {
-    $.post("storeInfo.php", {key: item});
+function storeInfo(x, item) {
+    $.post("storeInfo.php", {x: item});
 }
