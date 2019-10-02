56b735d3a18db8735e9b6f84b5ee9b2971e583ef
Jordan <jordan@burdett.us>
2019-10-02 17:18:48 -0600

    oops

--- index.php
+++ index.php
@@ -88,6 +88,7 @@
 
             <a href="shoppingCart.php" style="color:black;">
                 <div class="col-sm-1">
+                    <div class="shadow p-4 mb-4 bg-white">
                         <i class="fas fa-shopping-cart"></i>
                         <h4 style="text-align:center;">Cart</h4>
                     </div> 
