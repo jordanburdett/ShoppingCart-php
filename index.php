b2775604efb3e2b8b9c5a06fc5c1e6ffa8e2ef7d
Jordan <jordan@burdett.us>
2019-10-03 12:10:33 -0600

    test

--- index.php
+++ index.php
@@ -122,15 +122,17 @@
 /************
  * quick ajax request using jquery to store item to the session
  */
+
 function storeInfo(item) {
     $.post("storeInfo.php", {'0': item});
     showToast();
 }
 
-//shows toast when item is added
+ //shows toast when item is added
 function showToast() {
     $('.toast').toast('show');
 }
+
 </script>
 </body>
 
