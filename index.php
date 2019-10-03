3489467435c1f54ba945d380cea989b10f13ada4
Jordan <jordan@burdett.us>
2019-10-03 13:05:16 -0600

    test

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ session_start();
 
 <body>
 
-<div class="toast" data-delay="2000">
+<div class="toast" data-delay="10000">
     <div class="position-absolute w-100 d-flex flex-column p-4">
         <div class="toast ml-auto show" role="alert">
             <div class="toast-header">
