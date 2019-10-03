7fee29a51d5d097dd6b7d95bd211bf0f6308ebc0
Jordan <jordan@burdett.us>
2019-10-03 15:47:12 -0600

    test

--- index.php
+++ index.php
@@ -128,24 +128,6 @@ session_start();
     <a href="ShowSession.php">show session</a>
     </div>
 
-
-
-    <script>
-        /************
-         * quick ajax request using jquery to store item to the session
-         */
-
-        function storeInfo(item) {
-            $.post("storeInfo.php", {
-                '0': item
-            });
-
-            $('.toast').toast({
-                delay: 1000
-            });
-            $('.toast').toast('show');
-        }
-    </script>
 </body>
 
 </html>
--- javascript.js
+++ javascript.js
@@ -1,2 +1,15 @@
+/************
+         * quick ajax request using jquery to store item to the session
+         */
 
+        function storeInfo(item) {
+            $.post("storeInfo.php", {
+                '0': item
+            });
+
+            $('.toast').toast({
+                delay: 1000
+            });
+            $('.toast').toast('show');
+        }
 
