c7cda58d6876ce293c5d8859ca2e4f456d5a4545
Jordan <jordan@burdett.us>
2019-10-03 23:12:01 -0600

    test

--- javascript.js
+++ javascript.js
@@ -18,11 +18,11 @@
             console.log(item.value);
             console.log("." + item.id);
 
-            console.log(typeof(item.id));
+            console.log(typeof((item.id)));
 
             
             $.post("storeInfo.php", {
-                '0': item.id
+                '0': (item.id)
             });
             
 
